a820ec11a63da261bda8db49cd86e11d419d7bfa
caohaitao <caohaitao@sdiread.com>
2020-10-10 16:39:30 +0800

    去掉未使用的引用

--- src/pages/article/imageText/addImageText.jsx
+++ src/pages/article/imageText/addImageText.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { message, Button } from 'antd'
+import { message } from 'antd'
 import { getImageTextListApi } from 'api/article/imageText'
 
 class AddImageText extends Component {
@@ -22,10 +22,12 @@ class AddImageText extends Component {
 
     componentDidMount() {
         // console.log('this--====>>>>>', this)
-        // console.log('当前页面ID是 ====>>>', this.props.location.query.id)  此种方式传参 刷新页面 参数会丢失
+        // console.log('当前页面ID是 ====>>>', this.props.location.query.id)  // 此种方式传参 刷新页面 参数会丢失
         // console.log('当前页面ID是 ====>>>', this.props.location.state.id) // 此种方式传参 刷新页面 参数会丢失
-        console.log('当前页面ID是 ====>>>', this.props.location.search.split("?")[1]) //! 此种方式传参 刷新页面 参数 不会丢失
-        console.log('当前页面ID是 ====>>>', this.props.location.search.replace("?",'')) //! 此种方式传参 刷新页面 参数 不会丢失
+        this.props.location.query&&this.props.location.query.id && console.log('query 传参 当前页面ID是 ====>>>', this.props.location.query.id)
+        this.props.location.state&&this.props.location.state.id && console.log('state传参 当前页面ID是 ====>>>', this.props.location.state.id)
+        this.props.location.search&&console.log('Link传参 当前页面ID是 ====>>>', this.props.location.search.split("?")[1]) //! 此种方式传参 刷新页面 参数 不会丢失
+        this.props.location.search&&console.log('Link传参 当前页面ID是 ====>>>', this.props.location.search.replace("?",'')) //! 此种方式传参 刷新页面 参数 不会丢失
     }
     async getImageTextList() {
         try {
